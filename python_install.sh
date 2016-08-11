079942a998533a2a9fa467ef831437f9676d6d36
yyminmax <yyminmax@gmail.com>
2016-08-11 22:49:39 +0800

    Create python_install.sh

---
+++ python_install.sh
@@ -0,0 +1,21 @@
+wget url_from_python2.7
+
+tar -zxvf Python2.7.*.tgz
+
+cd Python2.7.*
+
+./configure --prefix=INSTALLPATH
+
+make 
+
+make install
+
+cd $INSTALLPATH
+
+echo $PATH
+
+export PATH=INSTALL/bin:$PATH
+
+export PYTHONPATH=/usr/lib/python2.7/dist-packages:$INSTALLPATH/lib/python2.7/site-packages
+
+pip install packages --install-option="--prefix=$INSTALLPATH/lib/python2.7/site-packages" --ignore-installed 
